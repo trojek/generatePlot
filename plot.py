232e12513d7855292193a238948accaa2924d2b3
Tomasz Rojek <tomek@rojek.cc>
2016-03-16 15:48:20 +0100

    add dots on the ends of sentences

--- plot.py
+++ plot.py
@@ -1,5 +1,5 @@
 """
-Prepare plots from data (file)
+Prepare plots from data (file).
 """
 
 import matplotlib.pyplot as plt
@@ -7,7 +7,7 @@ import sys
 
 
 def draw_plot(data, mean, median, title):
-    """Draw plot and save in on disk"""
+    """Draw plot and save in on disk."""
     plt.plot(data)
     plt.title('%s, mean %1.3f, median %1.3f' % (title, mean, median))
     plt.ylabel('aspect ratio: circularity')
@@ -16,20 +16,20 @@ def draw_plot(data, mean, median, title):
     plt.savefig('plots/%s.png' % title)
 
 def count_mean(data):
-    """Count mean value from data from file"""
+    """Count mean value from data from file."""
     data_copy = list(data)
     data_copy.sort()
     return sum(data_copy)/len(data_copy)
 
 def count_median(data):
-    """Count median value from data from file"""
+    """Count median value from data from file."""
     data_copy = list(data)
     data_copy.sort()
     return data_copy[(len(data_copy) / 2) + 1]
 
 
 def parse_data(file_name):
-    """Open file and prepare data further calculations"""
+    """Open file and prepare data further calculations."""
     with open("%s/%s.txt" % (file_name, file_name)) as dfile:
         data = dfile.read()
 
@@ -42,7 +42,7 @@ def parse_data(file_name):
     return cleared_data
 
 def main():
-    """Generates plot from text file"""
+    """Generates plot from text file."""
     data = parse_data(sys.argv[1])
     mean = count_mean(data)
     median = count_median(data)
